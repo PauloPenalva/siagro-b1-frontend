788a59d10a889e3e037201b782a16d0ef5cae1ae
Paulo Penalva <paulo.penalva@gmail.com>
2026-04-10 15:25:22 -0300

    commit

--- webapp/controller/parceirosNegocio/BaseController.ts
+++ webapp/controller/parceirosNegocio/BaseController.ts
@@ -1,5 +1,32 @@
+import Table from "sap/ui/table/Table";
 import CommonController from "../common/CommonController";
+import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
+import ODataModel from "sap/ui/model/odata/v4/ODataModel";
+import MessageBox from "sap/m/MessageBox";
+import Context from "sap/ui/model/odata/v4/Context";
 
 export abstract class BaseController extends CommonController {
-  
+
+  onAddAddress() {
+    const tb = this.byId("businessPartnerAddressesTable") as Table;
+    const oBinding = tb.getBinding("rows") as ODataListBinding;
+    oBinding.create({}, false, true, false);
+  }
+
+  onRemoveAddress() {
+    const oModel = this.getView().getModel() as ODataModel;
+    const oTable = this.byId("businessPartnerAddressesTable") as Table;
+    const aSelectedIndices = oTable.getSelectedIndices();
+
+    if (aSelectedIndices.length === 0) {
+      MessageBox.alert("Selecione um item para remover.");
+      return;
+    }
+
+    const index = aSelectedIndices[0];
+
+    const oContext = oTable.getContextByIndex(index) as Context;
+
+    void oContext.delete(oModel.getUpdateGroupId());
+  }
 }
